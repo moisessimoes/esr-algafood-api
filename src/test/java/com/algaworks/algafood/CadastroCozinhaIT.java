7541c637f9438fa96c15781678affcaebe46978c
moises.simoes <moises.simoes@unidac.com.br>
2022-09-30 21:52:00 -0300

    Inicio do modulo 10 - Testes de integração e de API

---
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -0,0 +1,49 @@
+package com.algaworks.algafood;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CadastroCozinhaIT {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Test
+	void deveRetornarStatus200_QuandoConsultarCozinhas() {
+		
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		RestAssured.given()
+				   .basePath("/cozinhas")
+				   .port(port)
+				   .accept(ContentType.JSON)
+				   .when()
+				   .get()
+				   .then()
+				   .statusCode(HttpStatus.OK.value());
+	}
+	
+	
+	@Test
+	void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+		
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+		RestAssured.given()
+				   .basePath("/cozinhas")
+				   .port(port)
+				   .accept(ContentType.JSON)
+				   .when()
+				   .get()
+				   .then()
+				   .body("", Matchers.hasSize(4))
+				   .body("nome", Matchers.hasItems("Brasileira", "Portuguesa"));
+	}
+}
